1021e609c41ee562796039358eda2bcd2db72d92
Nicolas Aguirre <in-aguirre@hotmail.com>
2022-05-16 10:57:15 -0500

    added firebase and tailwind

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,118 +1,149 @@
-import { useState } from "react";
-import { ChevronRightIcon, StarIcon } from "@heroicons/react/solid";
+import { Fragment, useState } from "react";
+import { Popover, Transition } from "@headlessui/react";
+import { MenuIcon, XIcon } from "@heroicons/react/outline";
+import { dbInstance } from "../../firebaseConfig";
+import { addDoc } from "firebase/firestore";
+
+const navigation = [
+  { name: "Product", href: "#" },
+  { name: "Features", href: "#" },
+  { name: "Marketplace", href: "#" },
+  { name: "Company", href: "#" },
+];
 
 export function SubscribeForm() {
   const [email, setEmail] = useState(null);
-  const submitEmail = () => {};
+  const [loading, setLoading] = useState(false);
+  const [notified, setNotified] = useState(false);
+
+  const handleEmail = async () => {
+    setLoading(true);
+    if (email !== null) {
+      try {
+        await addDoc(dbInstance, { email });
+        setLoading(false);
+        setNotified(true);
+      } catch (error) {
+        setLoading(false);
+        setNotified(false);
+      }
+
+      console.log("Header 18 | added document!");
+    }
+  };
+
   return (
-    <div className="bg-white pb-8 sm:pb-12 lg:pb-12">
-      <div className="pt-8 overflow-hidden sm:pt-12 lg:relative lg:py-48">
-        <div className="mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl lg:grid lg:grid-cols-2 lg:gap-24">
-          <div>
-            {/* <div>
-              <img
-                className="h-11 w-auto"
-                src="https://tailwindui.com/img/logos/workflow-mark.svg?color=indigo&shade=600"
-                alt="Workflow"
+    <div className="relative bg-white overflow-hidden">
+      <div
+        className="hidden lg:block lg:absolute lg:inset-0"
+        aria-hidden="true"
+      >
+        <svg
+          className="absolute top-0 left-1/2 transform translate-x-64 -translate-y-8"
+          width={640}
+          height={784}
+          fill="none"
+          viewBox="0 0 640 784"
+        >
+          <defs>
+            <pattern
+              id="9ebea6f4-a1f5-4d96-8c4e-4c2abf658047"
+              x={118}
+              y={0}
+              width={20}
+              height={20}
+              patternUnits="userSpaceOnUse"
+            >
+              <rect
+                x={0}
+                y={0}
+                width={4}
+                height={4}
+                className="text-gray-200"
+                fill="currentColor"
               />
-            </div> */}
-            <div className="mt-20">
-              <div>
-                <a href="#" className="inline-flex space-x-4">
-                  <span className="rounded bg-indigo-50 px-2.5 py-1 text-xs font-semibold text-indigo-600 tracking-wide uppercase">
-                    E-BOOK
+            </pattern>
+          </defs>
+          <rect
+            y={72}
+            width={640}
+            height={640}
+            className="text-gray-50"
+            fill="currentColor"
+          />
+          <rect
+            x={118}
+            width={404}
+            height={784}
+            fill="url(#9ebea6f4-a1f5-4d96-8c4e-4c2abf658047)"
+          />
+        </svg>
+      </div>
+
+      <div className="relative pt-6 pb-16 sm:pb-24 lg:pb-32">
+        <main className="mt-16 mx-auto max-w-7xl px-4 sm:mt-24 sm:px-6 lg:mt-32">
+          <div className="lg:grid lg:grid-cols-12 lg:gap-8">
+            <div className="sm:text-center md:max-w-2xl md:mx-auto lg:col-span-6 lg:text-left">
+              <h1>
+                <span className="block text-sm font-semibold uppercase tracking-wide text-gray-500 sm:text-base lg:text-sm xl:text-base">
+                  E-book Coming soon
+                </span>
+                <span className="mt-1 block text-4xl tracking-tight font-extrabold sm:text-5xl xl:text-6xl">
+                  <span className="block text-gray-900">
+                    Ultimate Guide to Google APIs
                   </span>
-                  <span className="inline-flex items-center text-sm font-medium text-indigo-600 space-x-1">
-                    <span>Book Development Starting Soon!</span>
-                    <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
+                  <span className="block text-indigo-600 pt-3">
+                    with React & Node.js
                   </span>
-                </a>
-              </div>
-              <div className="mt-6 sm:max-w-xl">
-                <h1 className="text-4xl font-extrabold text-gray-900 tracking-tight sm:text-5xl">
-                  The Ultimate Guide to Google APIs with React & Node.js
-                </h1>
-                <p className="mt-6 text-xl text-gray-500">
-                  Learn to manipulate Google APIs from scratch: Authentication,
-                  Spreadsheets Management, Google Calendar, token management,
-                  and more!
+                </span>
+              </h1>
+              <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">
+                Learn to manipulate Google APIs from scratch: Authentication,
+                Spreadsheets Management, Google Calendar, token management, and
+                more!
+              </p>
+              <div className="mt-8 sm:max-w-lg sm:mx-auto sm:text-center lg:text-left lg:mx-0">
+                <p className="text-base font-medium text-gray-900">
+                  Sign up to get notified when it’s ready.
                 </p>
-              </div>
-              <form action="#" className="mt-12 sm:max-w-lg sm:w-full sm:flex">
-                <div className="min-w-0 flex-1">
-                  <label htmlFor="hero-email" className="sr-only">
-                    Email address
+                <form action="#" method="POST" className="mt-3 sm:flex">
+                  <label htmlFor="email" className="sr-only">
+                    Email
                   </label>
                   <input
-                    id="hero-email"
                     type="email"
-                    className="block w-full border border-gray-300 rounded-md px-5 py-3 text-base text-gray-900 placeholder-gray-500 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+                    name="email"
+                    id="email"
+                    className="block w-full py-3 text-base rounded-md placeholder-gray-500 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:flex-1 border-gray-300"
                     placeholder="Enter your email"
+                    onChange={(e) => {}}
                   />
-                </div>
-                <div className="mt-4 sm:mt-0 sm:ml-3">
                   <button
+                    onClick={handleEmail}
                     type="submit"
-                    className="block w-full rounded-md border border-transparent px-5 py-3 bg-indigo-600 text-base font-medium text-white shadow hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:px-10"
+                    className="mt-3 w-full px-6 py-3 border border-transparent text-base font-medium rounded-md text-white bg-gray-800 shadow-sm hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:flex-shrink-0 sm:inline-flex sm:items-center sm:w-auto"
                   >
                     Notify me
                   </button>
-                </div>
-              </form>
-              {/* <div className="mt-6"> */}
-                {/* <div className="inline-flex items-center divide-x divide-gray-300"> */}
-                  {/* <div className="flex-shrink-0 flex pr-5">
-                    <StarIcon
-                      className="h-5 w-5 text-yellow-400"
-                      aria-hidden="true"
-                    />
-                    <StarIcon
-                      className="h-5 w-5 text-yellow-400"
-                      aria-hidden="true"
-                    />
-                    <StarIcon
-                      className="h-5 w-5 text-yellow-400"
-                      aria-hidden="true"
-                    />
-                    <StarIcon
-                      className="h-5 w-5 text-yellow-400"
-                      aria-hidden="true"
-                    />
-                    <StarIcon
-                      className="h-5 w-5 text-yellow-400"
-                      aria-hidden="true"
-                    />
-                  </div> */}
-                  {/* <div className="min-w-0 flex-1 pl-5 py-1 text-sm text-gray-500 sm:py-3"> */}
-                    {/* <span className="font-medium text-gray-900">
-                      Rated 5 stars
-                    </span>{" "}
-                    by over{" "}
-                    <span className="font-medium text-indigo-600">
-                      500 beta users
-                    </span> */}
-                  {/* </div> */}
-                {/* </div> */}
-              {/* </div> */}
+                </form>
+                <p className="mt-3 text-sm text-gray-500">
+                  We will only email you once the book is ready.
+                </p>
+              </div>
             </div>
-          </div>
-        </div>
-
-        <div className="sm:mx-auto sm:max-w-3xl sm:px-6">
-          <div className="py-12 sm:relative sm:mt-12 sm:py-16 lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
-            <div className="hidden sm:block">
-              <div className="absolute inset-y-0 left-1/2 w-screen bg-gray-50 rounded-l-3xl lg:left-80 lg:right-0 lg:w-full" />
-              {/* <svg
-                className="absolute top-8 right-1/2 -mr-3 lg:m-0 lg:left-0"
-                width={404}
-                height={392}
+            <div className="mt-12 relative sm:max-w-lg sm:mx-auto lg:mt-0 lg:max-w-none lg:mx-0 lg:col-span-6 lg:flex lg:items-center">
+              <svg
+                className="absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-8 scale-75 origin-top sm:scale-100 lg:hidden"
+                width={640}
+                height={784}
                 fill="none"
-                viewBox="0 0 404 392"
+                viewBox="0 0 640 784"
+                aria-hidden="true"
               >
                 <defs>
                   <pattern
-                    id="837c3e70-6c3a-44e6-8854-cc48c737b659"
-                    x={0}
+                    id="4f4f415c-a0e9-44c2-9601-6ded5a34a13e"
+                    x={118}
                     y={0}
                     width={20}
                     height={20}
@@ -129,21 +160,54 @@ export function SubscribeForm() {
                   </pattern>
                 </defs>
                 <rect
-                  width={404}
-                  height={392}
-                  fill="url(#837c3e70-6c3a-44e6-8854-cc48c737b659)"
+                  y={72}
+                  width={640}
+                  height={640}
+                  className="text-gray-50"
+                  fill="currentColor"
                 />
-              </svg> */}
+                <rect
+                  x={118}
+                  width={404}
+                  height={784}
+                  fill="url(#4f4f415c-a0e9-44c2-9601-6ded5a34a13e)"
+                />
+              </svg>
+              <div className="relative mx-auto w-full rounded-lg shadow-lg lg:max-w-md">
+                <button
+                  type="button"
+                  className="relative block w-full bg-white rounded-lg overflow-hidden focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                  <span className="sr-only">Watch our video to learn more</span>
+                  <img
+                    className="w-full"
+                    src="https://images.unsplash.com/photo-1556740758-90de374c12ad?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
+                    alt=""
+                  />
+                  <span
+                    className="absolute inset-0 w-full h-full flex items-center justify-center"
+                    aria-hidden="true"
+                  >
+                    <svg
+                      className="h-20 w-20 text-indigo-500"
+                      fill="currentColor"
+                      viewBox="0 0 84 84"
+                    >
+                      <circle
+                        opacity="0.9"
+                        cx={42}
+                        cy={42}
+                        r={42}
+                        fill="white"
+                      />
+                      <path d="M55.5039 40.3359L37.1094 28.0729C35.7803 27.1869 34 28.1396 34 29.737V54.263C34 55.8604 35.7803 56.8131 37.1094 55.9271L55.5038 43.6641C56.6913 42.8725 56.6913 41.1275 55.5039 40.3359Z" />
+                    </svg>
+                  </span>
+                </button>
+              </div>
             </div>
-            {/* <div className="relative pl-4 -mr-40 sm:mx-auto sm:max-w-3xl sm:px-0 lg:max-w-none lg:h-full lg:pl-12"> */}
-              {/* <img
-                className="w-full rounded-md shadow-xl ring-1 ring-black ring-opacity-5 lg:h-full lg:w-auto lg:max-w-none"
-                src="https://tailwindui.com/img/component-images/top-nav-with-multi-column-layout-screenshot.jpg"
-                alt=""
-              /> */}
-            {/* </div> */}
           </div>
-        </div>
+        </main>
       </div>
     </div>
   );
